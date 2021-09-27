402df8aa2d421d87790baa7571036db0ef99a430
Ali Sbane <ali.sbane.dev@gmail.com>
2021-09-27 21:35:49 +0100

    Finished

---
+++ js/app.js
@@ -0,0 +1,38 @@
+(function IIEF() {
+  function accordion() {
+    let boxs = document.querySelectorAll(".accordion-item");
+    let questions = document.querySelectorAll(".accordion-q");
+    let answers = document.querySelectorAll(".accordion-a");
+    let answerHeight = [];
+
+    answers.forEach((answer) => {
+      answerHeight.push(answer.scrollHeight);
+    });
+
+    questions.forEach((question, key) => {
+      question.addEventListener("click", (e) => {
+        let questionBoxParent = question.parentElement;
+        if (questionBoxParent.classList.contains("accordion-open")) {
+          questionBoxParent.classList.remove("accordion-open");
+          answers[key].style.height = "0px";
+        } else {
+          boxs.forEach((box, index) => {
+            if (box.classList.contains("accordion-open")) {
+              box.classList.remove("accordion-open");
+              answers[index].style.height = "0px";
+            }
+          });
+
+          questionBoxParent.classList.add("accordion-open");
+          answers[key].style.height = answerHeight[key] + "px";
+        }
+      });
+    });
+  }
+
+  window.addEventListener("load", init);
+
+  function init() {
+    accordion();
+  }
+})();
